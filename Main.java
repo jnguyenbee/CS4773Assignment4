3debe1c316fc5e405712c47aad5e3370d457cf6f
jnguyenbee <40246010+jnguyenbee@users.noreply.github.com>
2019-10-29 16:32:19 -0500

    Main Class

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	
+	public static void main(String [] args)
+	{
+		//elevator.press2();
+		//elevator.press1();
+		//elevator.press3();
+		//elevator.press3();
+		//elevator.press1(); 
+	}
+}
